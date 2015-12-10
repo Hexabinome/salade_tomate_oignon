506f92b07e37ef7b76ad4e6957971180702ec001
Mohamed El Mouctar HAIDARA <mohamed.haidara@insa-lyon.fr>
2015-12-10 15:05:00 +0100

    Ajout d'une classe qui permettra de générer des données pour l'interface graphique

---
+++ app/src/main/java/com/hexabinome/saladetomateoignon/Modele/Mock.java
@@ -0,0 +1,13 @@
+package com.hexabinome.saladetomateoignon.Modele;
+
+/**
+ * Cette classe permettra de générer des données pour l'interface graphique.
+ * Elle contiendra des méthodes statiques etc...
+ * @author Mohamed El Mouctar HAIDARA
+ */
+public final class Mock {
+
+    private Mock(){
+
+    }
+}
